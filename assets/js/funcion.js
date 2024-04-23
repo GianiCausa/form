ebd320aa6be3d715948794376e0ec458f783296d
gianinna <gi.causa@duocuc.cl>
2024-04-23 16:39:35 -0400

    formulario

---
+++ assets/js/funcion.js
@@ -0,0 +1,11 @@
+function sumar(){
+    /*declare dos variables v1 v2, tomare el elemento que en html tiene id valor1 valor2 */
+    var v1=parseInt(document.getElementById[valor1].value);
+    var v2=parseInt(document.getElementById[valor2].value);
+    /*declaro la variable suma en cero */
+    var suma=0;
+    /*tomare la accion donde la variable 1 se semara con la 2 */
+    suma=v1+v2;
+    /* */
+    document.getElementById("resultado").value=suma;
+}
